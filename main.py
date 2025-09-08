cc5bf30288cde72f8bc0c25c6ae7575b9079d4f4
sp4tn1c <152334741+sp4tn1c@users.noreply.github.com>
2025-09-09 00:29:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 nums, target = [2, 7, 11, 15], 9
 
 
-def targ(nums, target):
+def two_sum(nums, target):
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
+            if str(nums[i]) == nums[i] or str(nums[j]) == nums[j] or str(target) == target:
+                return 'Неверный тип данных'
+            else:
+                if int(nums[i]) != nums[i] or int(nums[j]) != nums[j] or int(target) != target:
+                    return 'Неверный тип данных'
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                    break
 
 
-print(targ(nums, target))
+print(two_sum(nums, target))
+
