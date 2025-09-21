975088613bdd4470547600f906219f6d8f54d4a8
sp4tn1c <152334741+sp4tn1c@users.noreply.github.com>
2025-09-21 17:03:17 +0300

    Update main2.py

--- lr2/main2.py
+++ lr2/main2.py
@@ -2,8 +2,9 @@
 Игра-угадайка (бинарный поиск)
 """
 
-def main(targets, value) -> tuple[int, int]: # функция реализации бинарного поиска
-    counter = 0 # счётчик количества сравнений бинарного поиска
+
+def main(targets, value): # функция реализации бинарного поиска
+    counter = 1 # счётчик количества сравнений бинарного поиска
     low, high, mid = 0, len(targets) - 1, len(targets) // 2
     while targets[mid] != value and low <= high: # цикл работает, пока искомое число не нашлось
         if value > targets[mid]:
@@ -20,18 +21,31 @@ def main(targets, value) -> tuple[int, int]: # функция реализаци
 
 
 def user_input(): # функция взаимодействия с пользователем
-    l_bord = int(input("Введите начало списка: ")) # пользователь задаёт начало списка ждя поиска
-    h_bord = int(input("Введите конец списка: ")) # пользователь задаёт конец списка ждя поиска
-    find_v = list(range(l_bord, h_bord + 1)) # список для нахождения искомого числа
-    value = int(input(f"Загадайте число от {l_bord} до {h_bord}: ")) # пользователь задаёт искомое число
+    while True:
+        try: # проверка на корректность введённых данных
+            l_bord = int(input("Введите начало диапазона: ")) # пользователь задаёт начало списка ждя поиска
+            h_bord = int(input("Введите конец диапазона: ")) # пользователь задаёт конец списка ждя поиска
+            if l_bord > h_bord: # некорректный список
+                print('Некорректный диапазон')
+                break
+            find_v = list(range(l_bord, h_bord + 1)) # список для нахождения искомого числа
+            value = int(input("Загадайте число: ")) # пользователь задаёт искомое число
+            if h_bord < value or l_bord > value:
+                print(f'Число не внутри диапазона от {l_bord} до {h_bord}') # проверка на принадлежность числа к диапазону
+                break
+            index, counter = main(find_v, value) # вызов функции main() для реализации бинарного поиска для вводных данных
+            if index:
+                print(f"Число {find_v[index]}")
+                print(f"Количество сравнений: {counter}")
+            else:
+                print("Число не найдено")
+            break
+        except ValueError: # проверка на тип данных
+            print('Число не является целым')
+            break
 
-    index, counter = main(find_v, value) # вызов функции main() для реализации бинарного поиска для вводных данных
 
-    if index:
-        print(f"Число {find_v[index]}")
-        print(f"Количество сравнений: {counter}")
-    else:
-        print("Число не найдено.")
+user_input() # вызов функции для реализации игры-угадайки 
 
 
 user_input()
