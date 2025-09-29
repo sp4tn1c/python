4325c295a9fc6f499a90e1fd81953d6c4509a544
sp4tn1c <152334741+sp4tn1c@users.noreply.github.com>
2025-09-29 23:56:02 +0300

    Update test2.py

--- lr2/test2.py
+++ lr2/test2.py
@@ -7,19 +7,24 @@ class TestGuessingGame(unittest.TestCase):
 
     def test_guess1(self): # загаданное число в середине диапазона
         self.assertEqual(main([1, 2, 3, 4, 5], 3), (2, 1))
+        
     def test_guess2(self): # загаданное число в начале диапазона
         self.assertEqual(main([9, 10, 11, 12, 13], 9), (0, 2))
+        
     def test_guess3(self): # большой диапазон
-        self.assertEqual(main(list(range(1, 100)), 56), (55, 4))
+        self.assertEqual(main(list(range(1, 100)), 56), (55, 4)) 
+        
     def test_guess4(self): # левая граница больше правой
         self.assertEqual(main([5, 4, 3, 2, 1], 5), None)
+        
     def test_guess5(self): # наличие строкового типа данных
         self.assertEqual(main(['a', '2', 3, 4, 5], 4), (3, 2))
+        
     def test_guess6(self): # наличие типа данных float
         self.assertEqual(main([1.1, 2.2, 3.3, 4.4, 5.5], 4), None)
+        
     def test_guess7(self): # числа нет в диапазоне
         self.assertEqual(main([1, 2, 3, 4, 5], 6), None)
-
 
 
 if __name__ == '__main__':
