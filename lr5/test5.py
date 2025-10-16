06b65385b39e2088d790bb86ed0489126883a0a5
sp4tn1c <152334741+sp4tn1c@users.noreply.github.com>
2025-10-16 14:10:17 +0300

    Update test5.py

--- lr5/test5.py
+++ lr5/test5.py
@@ -1,1 +1,30 @@
+from lab5 import gen_bin_tree
+import unittest
 
+
+class TestBinaryTree(unittest.TestCase):
+
+    def test_binary1(self): # проверка построения дерева с height = 0
+        self.assertEqual(gen_bin_tree(0, 3), {3})
+
+    def test_binary2(self): # проверка построения дерева с height > 0
+        self.assertEqual(gen_bin_tree(1, 10), {10: [15.0, 100]})
+
+    def test_simple3(self): # проверка построения дерева с нецелым height
+        self.assertEqual(gen_bin_tree(0.5, 2), 'Неверный тип данных')
+
+    def test_simple4(self): # проверка построения дерева с отрицательным height
+        self.assertEqual(gen_bin_tree(-1, 2), 'Высота и корень дерева не могут быть отрицательными')
+
+    def test_simple5(self): # проверка построения дерева с отрицательным root
+        self.assertEqual(gen_bin_tree(1, -2), 'Высота и корень дерева не могут быть отрицательными')
+
+    def test_simple6(self): # проверка построения дерева с отрицательным root
+        self.assertEqual(gen_bin_tree('a', 3), 'Неверный тип данных')
+
+    def test_simple7(self): # проверка построения дерева с root, не являющегося числом
+        self.assertEqual(gen_bin_tree(3, 'a'), 'Неверный тип данных')
+
+
+if __name__ == '__main__':
+    unittest.main()
