42ecc05581c89c402af9aeb74b43d430a83b2946
sp4tn1c <152334741+sp4tn1c@users.noreply.github.com>
2025-12-05 14:19:59 +0300

    Update setUp-tearDown.py

--- lr7/setUp-tearDown.py
+++ lr7/setUp-tearDown.py
@@ -9,21 +9,33 @@ class TestStreamWrite(unittest.TestCase):
   def setUp(self):
     self.nonstandardstream = io.StringIO()
 
+
     try:
-      self.get_currencies = get_currencies(['USD'],
+        self.get_currencies = get_currencies(['USD'],
                                            url="https://",
                                            handle=self.nonstandardstream)
     except:
-      pass
+        self.trace = trace(get_currencies, handle=self.nonstandardstream)
+
+def break_stream(self): # проверка сломанного потока при ошибочном API
+
+    broken_stream = io.StringIO()
+    broken_stream.write('assert_phrase_error')
+
+    with self.assertRaises(requests.eceptions.RequestEception):
+        get_currencies(['USD'], url="https://", broken_stream=broken_stream)
+
+    regex = broken_stream.getvalue()
+    self.assertRegex(regex, r'Ошибка получения API')
 
 
-  def test_writing_stream(self):
+
+def test_writing_stream(self):
     
-      output = self.nonstandardstream.getvalue()
+    output = self.nonstandardstream.getvalue()
+    self.assertRegex(output, r"Ошибка при запросе к API")
 
-      self.assertRegex(output, r"Ошибка при запросе к API")
-
-  def tearDown(self):
+def tearDown(self):
 
     del self.nonstandardstream
 
